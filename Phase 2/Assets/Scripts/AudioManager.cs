a4c1aced16d950442789b72519f84ac2d9d86415
Andrew Tanti <dru.tanti@gmail.com>
2019-06-04 14:25:31 +0200

    Finalised Player
    + Finalised animation and particle effects for the player character
    + Cleaned up file structure to be more oranised
    - Removed Test level
    - Removed old animation sprites from player character

---
+++ Phase 2/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager current { get; private set; }
+
+    void Awake()
+    {
+        // Check that the instance for GameManager exists, if not set to this class.
+        if (current == null)
+        {
+            current = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            DestroyImmediate(gameObject);
+            return;
+        }
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
