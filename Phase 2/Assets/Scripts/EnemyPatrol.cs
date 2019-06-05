0f0d42db131ff8ae817aef046e4880335efdf6f9
Andrew Tanti <dru.tanti@gmail.com>
2019-06-05 07:19:06 +0200

    Ant AI

--- Phase 2/Assets/Scripts/EnemyPatrol.cs
+++ Phase 2/Assets/Scripts/EnemyPatrol.cs
@@ -9,6 +9,11 @@ public class EnemyPatrol : MonoBehaviour
     public Transform patrolStart;
     public Transform patrolEnd;
     private Vector2 target;
+    public Transform pickaxeSpawn;
+    public GameObject pickaxe;
+    public float distance;
+
+
 
     private void Start() 
     {
@@ -16,6 +21,27 @@ public class EnemyPatrol : MonoBehaviour
     }
 
     void Update()
+    {
+        Move();
+        PlayerSearch();
+    }
+
+    void ThrowPickaxe()
+    {
+        Instantiate(pickaxe, pickaxeSpawn.position, pickaxeSpawn.rotation);
+    }
+
+    void PlayerSearch()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.right, distance);
+        if(hitInfo.collider != null) {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+        } else {
+            Debug.DrawLine(transform.position, transform.position + transform.right * distance, Color.green);
+        }
+    }
+
+    void Move()
     {
         transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
         if (transform.position.x == target.x && transform.position.y == target.y)
---
+++ Phase 2/Assets/Scripts/Pickaxe.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickaxe : MonoBehaviour
+{
+    public float speed;
+    public float torque;
+
+    private Rigidbody2D _pickaxeRB;
+
+    private void Awake()
+	{
+		// We're looking for the Rigidbody2D component
+		// on this object, and taking reference.
+		_pickaxeRB = GetComponent<Rigidbody2D>();
+	}
+
+    private void Start()
+	{
+		_pickaxeRB.velocity = transform.TransformDirection(Vector2.up) * speed;
+		
+		_pickaxeRB.AddTorque(torque, ForceMode2D.Impulse);
+	}
+}
