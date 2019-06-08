a7868a59b310b97cc4ac16cc304ab90873d56406
Andrew Tanti <dru.tanti@gmail.com>
2019-06-08 22:11:41 +0200

    Revert "Merge branch 'Level-Assembly'"
    This reverts commit 935027d0488bb5c506f4b656ec96dce1f85a767d, reversing
    changes made to 82c42157294c4a6da40b9222beea5d196c9851a0.

--- Phase 2/Assets/Scripts/Gamemanager.cs
+++ Phase 2/Assets/Scripts/Gamemanager.cs
@@ -1,60 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    // Defines the AudioManager as a singleton.
+    public static GameManager current { get; private set; }
 
-    public static bool GameIsPaused = false;
-    public GameObject pauseMenuUI;
-
-    void Update()
+    void Awake()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        // Check that the instance for GameManager exists, if not set to this class.
+        if (current == null)
         {
-            if (GameIsPaused)
-            {
-                Resume();
-            } else
-            {
-                Pause();
-            }
+            current = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            DestroyImmediate(gameObject);
+            return;
         }
-    }
-
-  public void PlayGame ()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    public void Resume()
-    {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        GameIsPaused = false;
-    }
-
-    void Pause()
-    {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        GameIsPaused = true;
-    }
-
-    public void LoadMenu()
-    {
-        SceneManager.LoadScene("MainMenu");
-        Time.timeScale = 1f;
-    }
-
-    public void Restart()
-    {
-        SceneManager.LoadScene("ManuelLevel");
-    }
-
-    public void QuitGame ()
-    {
-        Application.Quit();
     }
 }
